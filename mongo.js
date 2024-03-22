ce42a5b6953b922b8b73feda8c4d2389a71e1154
SebastianS <seb.kontakt.sob@gmail.com>
2024-03-22 10:41:34 +0100

    xd

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]mongodb+srv://sebkontaktsob:${password}orleann.ylzcx4r.mongodb.net/
+const url = `mongodb+srv://sebkontaktsob:${password}orleann.ylzcx4r.mongodb.net/`
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(url, { dbName: 'phonebook' })
+  .then(console.log('Connected to DB'))
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person.save().then(result => {
+    console.log(`Added ${person.name} ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
